23705a65ece38c50378c60c07194c1037f7d6641
CKentarou <k023c0018@g.neec.ac.jp>
2025-04-08 17:23:18 +0900

    Initial Commit

---
+++ src/test/java/com/example/ReTask/ReTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
