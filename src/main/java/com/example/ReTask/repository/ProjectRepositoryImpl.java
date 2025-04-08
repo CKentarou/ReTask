8f9b0b9d2fedce8005191d1e7d43008cb219dbaa
CKentarou <k023c0018@g.neec.ac.jp>
2025-04-08 20:03:54 +0900

    Project一覧データ取得用Repositoryの作成と実装

---
+++ src/main/java/com/example/ReTask/repository/ProjectRepositoryImpl.java
@@ -0,0 +1,36 @@
+package com.example.ReTask.repository;
+
+import com.example.ReTask.entity.Project;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class ProjectRepositoryImpl implements ProjectRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Project> findAll() {
+        String sql = "SELECT * FROM projects";
+
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+
+        List<Project> result = new ArrayList<Project>();
+        for (Map<String, Object> one : list) {
+            Project project = new Project();
+            project.setProjectId((int)one.get("id"));
+            project.setProjectName((String)one.get("name"));
+            project.setProjectDescription((String)one.get("description"));
+
+            result.add(project);
+        }
+
+        return result;
+    }
+}
