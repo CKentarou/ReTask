70b280c4a652061e6f98d70193f219bcd415b67b
CKentarou <k023c0018@g.neec.ac.jp>
2025-04-09 09:46:06 +0900

    projectCreateの引数でFormを渡す

--- src/main/java/com/example/ReTask/controller/ProjectController.java
+++ src/main/java/com/example/ReTask/controller/ProjectController.java
@@ -1,12 +1,14 @@
 package com.example.ReTask.controller;
 
 import com.example.ReTask.entity.Project;
+import com.example.ReTask.form.ProjectForm;
 import com.example.ReTask.service.ProjectListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 
@@ -27,8 +29,10 @@ public class ProjectController {
     }
 
     @GetMapping("/project/create")
-    public String projectCreate(Model model) {
+    public String projectCreate(@ModelAttribute ProjectForm form, Model model) {
         model.addAttribute("title", "プロジェクト作成");
         return "project-create";
     }
+
+
 }
