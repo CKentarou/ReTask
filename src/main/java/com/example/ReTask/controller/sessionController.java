b4706bc304be65af6b0f2293f338a4cc91f8c3a0
CKentarou <k023c0018@g.neec.ac.jp>
2025-04-10 09:21:57 +0900

    sessionIdを渡す＆sessionをDBから取得する処理の追加

--- src/main/java/com/example/ReTask/controller/sessionController.java
+++ src/main/java/com/example/ReTask/controller/sessionController.java
@@ -2,23 +2,42 @@ package com.example.ReTask.controller;
 
 import com.example.ReTask.entity.Session;
 import com.example.ReTask.form.SessionInitForm;
-import com.example.ReTask.service.SessionInitService;
+import com.example.ReTask.service.SessionGetService;
+import com.example.ReTask.service.SessionInsertService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequiredArgsConstructor
+
 public class sessionController {
-    private final SessionInitService initService;
+    private final SessionInsertService initService;
+    private final SessionGetService sessionGetService;
+
+    public sessionController(SessionInsertService initService, SessionGetService sessionGetService) {
+        this.initService = initService;
+        this.sessionGetService = sessionGetService;
+    }
 
     @PostMapping("/new-session")
-    public String showSession(@ModelAttribute SessionInitForm form, Model model) {
+    public String showSession(@ModelAttribute SessionInitForm form, RedirectAttributes redirectAttributes, Model model) {
         Session session = form.toSession(form.getSessionCount());
-        initService.initSession(session);
+        int sessionId = initService.initSession(session);
+        redirectAttributes.addFlashAttribute("sessionId", sessionId);
+        return "redirect:/new-session";
+    }
+
+    @GetMapping("/new-session")
+    public String showSessionPage(Model model) {
+        //作成したsessionをDBから取得してモデルに追加する
+        Integer sessionId = (Integer) model.getAttribute("sessionId");  //sessionIdを取得
+        Session session = sessionGetService.getSessionBySessionId(sessionId);
+        System.out.println(session);
+        model.addAttribute("session", session);
         return "new-session";
     }
 }
